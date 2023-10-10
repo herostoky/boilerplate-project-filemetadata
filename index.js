f3ec9dfa87039628fe40c337bcc323900144b560
Toky <Toky.SOLOFONIRINA@pulse.mg>
2023-10-10 14:49:08 +0300

    add file upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+let express = require('express');
+let cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const multer  = require('multer')
+const upload = multer({ dest: './public/files/' })
+
+let app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  let fileUp = req.file;
+  let filename = fileUp.originalname;
+  let fileType = fileUp.mimetype;
+  let fileSize = fileUp.size;
+  res.json({
+    name: filename,
+    type: fileType,
+    size: fileSize,
+  });
+  // {"name":"Screenshot from 2023-10-10 08-43-00.png","type":"image/png","size":32146}
+});
 
 
 const port = process.env.PORT || 3000;
